c0823cff54377b592868768de93ff958c7d82731
Nurbosh <78221258+Nurbosh@users.noreply.github.com>
2022-11-26 01:03:18 +0600

    Add files via upload

---
+++ sendform.php
@@ -0,0 +1,25 @@
+<?php
+//Сбор данных из полей формы. 
+$name = $_POST['name'];// Берём данные из input c атрибутом name="name"
+$phone = $_POST['phone']; // Берём данные из input c атрибутом name="phone"
+
+
+$token = "5973898427:AAFXXiwTjQbcM1pWhjC0DtjDWHDabtQet-I"; // Тут пишем токен
+$chat_id = "-862923127"; // Тут пишем ID группы, куда будут отправляться сообщения
+$sitename = "nurbosh.github.io"; //Указываем название сайта
+
+$arr = array(
+
+  'Заказ с сайта: ' => $sitename,
+  'Имя: ' => $name,
+  'Телефон: ' => $phone,
+  'Почта' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+?>
